5ad41f8b3e25d7f405fa91ef7cbccce5fda4af8f
Sinhad11 <119247465+Sinhad11@users.noreply.github.com>
2025-01-03 13:12:59 -0600

    Hospital Management System
    Hospital Management System .Created by Riad Ahmed Sinhad

---
+++ scriptUI.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').addEventListener('click', function() {
+    alert('Button 1 clicked!');
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    alert('Button 2 clicked!');
+});
+
+
+
+
